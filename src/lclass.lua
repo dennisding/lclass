1ff7e55543b3a1474f3dca25981f03a14ca9733b
dennis <functionalx@163.com>
2024-03-26 13:00:50 +0800

    add _super fun

--- src/lclass.lua
+++ src/lclass.lua
@@ -6,6 +6,32 @@ local function print_table(t)
     end
 end
 
+function _super(type, instance)
+    local wraper = {}
+
+    local function _index(t, key)
+        local super_method = nil
+        while type._super ~= nil do
+            super_method = type._super[key]
+            if super_method ~= nil then
+                break
+            end
+        end
+        local function _method(...)
+            if super_method ~= nil then
+                return super_method(instance, ...)
+            end
+        end
+
+        return _method
+    end
+
+    local meta = {__index = _index }
+    setmetatable(wraper, meta)
+
+    return wraper
+end
+
 function _class(name, super)
     local _new_class = {_type_name = name, _super = super}
     _new_class.__index = _new_class
@@ -37,15 +63,27 @@ function Person:_init(name, age)
 end
 
 function Person:say_hello()
-    print("name:", self.name, "say hello")
+    print("Person:", self.name, "say hello")
 end
 
-local new_person = Person("dennis", 43)
+local Student = _class("Student", Person)
 
-print_table(new_person)
+function Student:_init(name, age, grade)
+    _super(Student, self)._init(name, age)
+
+    self.grade = grade
+    print('student init')
+end
+
+function Student:say_hello()
+    _super(Student, self).say_hello()
+    print('student say hello')
+end
+
+local new_person = Student("dennis", 43, 3)
+
+-- print_table(new_person)
 
 new_person:say_hello()
 
 print('new person', new_person)
-
-print('hello x')
